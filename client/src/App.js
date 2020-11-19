8795dc852c590ae0bffbbb2b2e8dc00fe8a0afea
skb15428 <51283207+skb15428@users.noreply.github.com>
2020-11-20 01:47:41 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./components/navbar.component.js"
+import ExercisesList from "./components/excercises-list.component";
+import EditExercise from "./components/edit-excercise.component";
+import CreateExercise from "./components/create-excercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App(){
+    return (
+        <Router>
+            <Navbar/>
+            <br/>
+            <div className="container">
+                <Route path="/" exact component={ExercisesList}/>
+                <Route path="/edit/:id" component={EditExercise}/>
+                <Route path="/create" component={CreateExercise}/>
+                <Route path="/user" component={CreateUser}/>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
